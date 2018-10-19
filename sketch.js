2a912720ddb753096b4daabc11aadd3bc2aa7347
bambusmalpa <41670099+bambusmalpa@users.noreply.github.com>
2018-10-19 18:50:17 +0200

    chart added

--- sketch.js
+++ sketch.js
@@ -1,18 +1,28 @@
 let canvSize = 400;
 let canvOrigin = canvSize / 2;
-let arrSize = 500;
+let arrSize = 10000;
 let points = [];
 let iterator = 0;
 let conTrue = 0;
 let monitor;
 let start;
 let sampleInput;
+let action = false;
+let currentPi;
+let piArr = [];
 
 function setup() {
     createCanvas(canvSize, canvSize);
     monitor = createP("");
     start = createButton("START");
-    sampleInput = createInput("PODAJ LICZBĘ");
+    sampleInput = createInput(arrSize);
+    start.mouseClicked(function () {
+        if (action == false) {
+            action = true;
+        } else {
+            action = false;
+        }
+    })
     createRandomPoints();
 
 
@@ -30,7 +40,7 @@ let createRandomPoints = () => {
 function draw() {
     drawBoard();
     drawPoints();
-
+    chartUpdate();
     if (iterator >= arrSize) {
         noLoop();
     }
@@ -39,18 +49,40 @@ let drawBoard = () => {
     noFill();
     stroke(10);
     ellipse(canvSize / 2, canvSize / 2, canvSize, canvSize);
-    rect(0, 0, canvSize - 1, canvSize - 1);
 }
 
 let drawPoints = () => {
-    fill(0);
-    noStroke();
-    if (dist(points[iterator][0], points[iterator][1], canvOrigin, canvOrigin) <= canvOrigin) {
+    if (action) {
+        fill(0);
+        noStroke();
+        if (dist(points[iterator][0], points[iterator][1], canvOrigin, canvOrigin) <= canvOrigin) {
 
-        fill(255, 0, 0)
-        conTrue++
+            fill(255, 0, 0)
+            conTrue++
+        }
+        ellipse(points[iterator][0], points[iterator][1], 3, 3)
+        iterator++;
+        currentPi = conTrue / iterator * 4;
+        monitor.html(currentPi);
+        piArr.push(currentPi);
+
     }
-    ellipse(points[iterator][0], points[iterator][1], 3, 3)
-    iterator++;
-    monitor.html(conTrue / iterator * 4)
 }
+let chartUpdate = () => {
+    return currentPi;
+}
+
+
+
+Plotly.plot('chart', [{
+    y: [chartUpdate()],
+    type: 'line'
+}]);
+
+setInterval(function () {
+    Plotly.extendTraces('chart', {
+        y: [
+            [chartUpdate()]
+        ]
+    }, [0])
+}, 200);
